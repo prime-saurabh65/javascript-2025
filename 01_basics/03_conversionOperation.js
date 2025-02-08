5cbc7692ceaaee4ead7ac8457ff10ebfc5959ade
Saurabh Shivam <72144347+prime-saurabh65@users.noreply.github.com>
2025-02-08 10:43:52 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,3 +25,12 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+// cntr + B : use for the show files.
+// cntr + ~ : use for the open and closed terminal.
+// cntr + P : use for the quick open menu.
+// cntr + \ : use for split editor window.
+// cntr + W : use for the closed file.
+// cntr + shift + l : use select all matches and then edit or del.
+// cntr + D : use for add selection to matches.
+// shift + alt + down : use for the duplicates lines and paste it there down.
